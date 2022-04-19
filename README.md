84426f98f978b82a70ad7a4f4a684e977d9255a6
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-04-20 02:03:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# HR leave management system in Java and Spring
+## Overview
+Practice project to learn SOLID principle
+
+## Used technologies
+* Java 17
+* Spring Boot Reactive
+* MariaDB
---
+++ src/main/java/kz/sabyrzhan/hrleavemanagement/HrLeaveManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package kz.sabyrzhan.hrleavemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrLeaveManagementSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrLeaveManagementSpringApplication.class, args);
+    }
+
+}
